7ab6903d93e7d8884fc3c47bd4919346abe9e2c0
DhruvDave <dhruvdave596@gmail.com>
2024-09-09 12:34:45 +0530

    Product Moule Crud Working

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,72 +5,145 @@ using System.Data;
 
 namespace Nice_admin_form___table.Controllers
 {
-
     public class ProductController : Controller
     {
-
-
-        private IConfiguration configuration;
+        private readonly IConfiguration configuration;
 
         public ProductController(IConfiguration _configuration)
         {
             configuration = _configuration;
         }
 
-
-
+        // Display the product list
         public IActionResult ProductList()
         {
+            List<ProductModel> productList = new List<ProductModel>();
+
             string connectionString = configuration.GetConnectionString("ConnectionString");
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            SqlCommand command = connection.CreateCommand();
-            command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "GetProducts";
-            SqlDataReader reader = command.ExecuteReader();
-            DataTable table = new DataTable();
-            table.Load(reader);
-            connection.Close();  // It's a good practice to close the connection when done.
-
-            return View(table);
-        }
-
-        public IActionResult AddProductForm()
-        {
-            return View();
-        }
-
-        public IActionResult ProductDelete(int ProductID)
-        {
-            try
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "GetProducts";  // Procedure to get all products
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    productList.Add(new ProductModel
+                    {
+                        ProductID = (int)reader["ProductID"],
+                        ProductName = reader["ProductName"].ToString(),
+                        ProductPrice = (decimal)reader["ProductPrice"],
+                        ProductCode = reader["ProductCode"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        UserID = (int)reader["UserID"]
+                    });
+                }
+            }
 
+            return View(productList);  // Pass the product list to the view
+        }
+
+        // Add/Edit form view
+        public IActionResult AddEditProductForm(int? ProductID)
+        {
+            ProductModel product = new ProductModel();
+            List<UserModel> users = new List<UserModel>();
+
+            // Fetch Users for Dropdown
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand userCommand = new SqlCommand("SELECT UserID, UserName FROM dbo.New_User", connection);
+                SqlDataReader userReader = userCommand.ExecuteReader();
+                while (userReader.Read())
+                {
+                    users.Add(new UserModel
+                    {
+                        UserID = (int)userReader["UserID"],
+                        UserName = userReader["UserName"].ToString()
+                    });
+                }
+            }
+            ViewBag.Users = users;  // Pass users to the view for the dropdown
+
+            if (ProductID.HasValue)
+            {
+                // Editing an existing product
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-
-                    using (SqlCommand command = connection.CreateCommand())
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "GetProducts";  // Fetch product by ID
+                    command.Parameters.AddWithValue("@ProductID", ProductID);
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
                     {
-                        command.CommandType = CommandType.StoredProcedure;
-                        command.CommandText = "DeleteProduct";
-                        command.Parameters.Add("@ProductID", SqlDbType.Int).Value = ProductID;
-
-                        command.ExecuteNonQuery();
+                        product.ProductID = (int)reader["ProductID"];
+                        product.ProductName = reader["ProductName"].ToString();
+                        product.ProductPrice = (decimal)reader["ProductPrice"];
+                        product.ProductCode = reader["ProductCode"].ToString();
+                        product.Description = reader["Description"].ToString();
+                        product.UserID = (int)reader["UserID"];
                     }
                 }
             }
-            catch (Exception ex)
+
+            return View("AddEditProduct", product);
+        }
+
+        // Save product (Insert or Update)
+        [HttpPost]
+        public IActionResult SaveProduct(ProductModel product)
+        {
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                TempData["ErrorMessage"] = "Error deleting product: " + ex.Message;
-                // You can also log the error using a logging framework, if available.
-                Console.WriteLine(ex.ToString());
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+
+                if (product.ProductID > 0)
+                {
+                    command.CommandText = "UpdateProduct";  // Update existing product
+                    command.Parameters.AddWithValue("@ProductID", product.ProductID);
+                }
+                else
+                {
+                    command.CommandText = "InsertProduct";  // Insert new product
+                }
+
+                command.Parameters.AddWithValue("@ProductName", product.ProductName);
+                command.Parameters.AddWithValue("@ProductPrice", product.ProductPrice);
+                command.Parameters.AddWithValue("@ProductCode", product.ProductCode);
+                command.Parameters.AddWithValue("@Description", product.Description);
+                command.Parameters.AddWithValue("@UserID", product.UserID);
+
+                command.ExecuteNonQuery();
             }
 
             return RedirectToAction("ProductList");
         }
 
+        // Delete product
+        public IActionResult ProductDelete(int ProductID)
+        {
+            string connectionString = configuration.GetConnectionString("ConnectionString");
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "DeleteProduct";
+                command.Parameters.AddWithValue("@ProductID", ProductID);
+                command.ExecuteNonQuery();
+            }
 
+            return RedirectToAction("ProductList");
+        }
     }
 }
--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -4,9 +4,15 @@
     {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
-        public double ProductPrice { get; set; }
+        public decimal ProductPrice { get; set; }
         public string ProductCode { get; set; }
         public string Description { get; set; }
         public int UserID { get; set; }
     }
+}
+
+public class UserModel
+{
+    public int UserID { get; set; }
+    public string UserName { get; set; }
 }
